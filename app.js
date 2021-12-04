b1e65b5831029b5c5ee0cc51142a3939c57befdc
Darshan Bajeja <codingdsb@gmail.com>
2021-12-04 22:20:35 +0530

    handle 404

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const main = () => {
   app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+  app.use((req, res, next) => {
+    res.status(404).json({
+      error: 404,
+    });
+  });
 
   mongoose
     .connect(MONGO_URI, {
