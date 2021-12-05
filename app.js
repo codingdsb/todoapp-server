cf2ebf8071b0fdb8ed70b4943a931edc17df59aa
Darshan Bajeja <codingdsb@gmail.com>
2021-12-05 10:56:43 +0530

    bug fixs

--- app.js
+++ app.js
@@ -8,12 +8,6 @@ const main = () => {
   app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.use((req, res, next) => {
-    res.status(404).json({
-      error: "Not found",
-    });
-  });
-  app.use(express.static("public"));
 
   mongoose
     .connect(MONGO_URI, {
@@ -38,6 +32,12 @@ const main = () => {
       availableRoutes: ["/api/user/...", "/api/todo/..."],
     });
   });
+  app.use((req, res, next) => {
+    res.status(404).json({
+      error: "Not found",
+    });
+  });
+  app.use(express.static("public"));
 };
 
 main();
